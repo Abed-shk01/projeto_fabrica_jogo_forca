3e32b573b57524e5880ad0352565f6967a35a029
Abed Sheikhi <shabed.1389@gmail.com>
2025-09-19 16:20:12 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,66 @@
+# Criar um jogo de forca onde o usuario tenta adicionar uma palavra secreta
+# letra por letra, com até 6 erros permitidos.
+
+import random # biblioteca que escolhe aleatoriamente a palavra 
+
+def jogar():
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas=inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
+
+    enforcou = False 
+    acertou =False 
+    erros = 0
+
+    while(not enforcou and not acertou):
+        chute = pede_chute()
+        
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas [index] = letra
+                index +=1
+        else:
+                erros +=1
+        enforcou = erros ==6 
+        acertou ='_' not in letras_acertadas
+        print(letras_acertadas)
+    if acertou:
+        print('Você ganhou!')
+    else:
+        print('Você perdeu.')
+    print('Fim do jogo.')
+
+
+def pede_chute():
+        chute = input("Qual a letra?  ")
+        chute = chute.strip().lower()
+        return chute
+def imprime_mensagem_abertura():
+        print('****************************************')
+        print('******* bem vindo ao jogo de forca ! ********')
+        print('****************************************')
+
+
+def carrega_palavra_secreta(nome_do_arquivo= r'C:\Users\Aluno_Programador2\Desktop\Jogo\forca.txt'):
+        arquivo = open(nome_do_arquivo, 'r')
+        palavras = []
+
+        for linha in arquivo:
+            linha =linha.strip()
+            palavras.append(linha)
+
+            arquivo.close()
+
+            numero = random.randrange(0, len(palavras))
+            palavra_secreta = palavras[numero].lower()
+
+            return palavra_secreta
+        
+def inicializa_letras_acertadas(palavra):
+            return['_'for letra in palavra]
+        
+if __name__ == '__main__':
+            jogar()
